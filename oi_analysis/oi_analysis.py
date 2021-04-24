b3ff922bf8f7720c75f6fdbefb44e92ee0ab9cc6
abasava <basava.akula@gmail.com>
2021-04-25 00:34:02 +0530

    1. Minot improvements in oi_analysis gui

--- oi_analysis/oi_analysis.py
+++ oi_analysis/oi_analysis.py
@@ -16,9 +16,9 @@ class OI_ANALYSIS:
     def setup_gui(self,my_master: Frame)->None:
         self.master_wd = my_master
         top_frame: Frame = Frame(my_master)
-        top_frame.pack(anchor='nw',fill='y', expand=False, side=LEFT)
+        top_frame.pack(anchor='nw',fill='y', expand=False, side=TOP)
         bot_frame: Frame = Frame(my_master)
-        bot_frame.pack(fill='both', expand=True, side=LEFT)
+        bot_frame.pack(fill='both', expand=True, side=TOP)
         pdx = 1
         pdy = 1
         
@@ -27,43 +27,38 @@ class OI_ANALYSIS:
         var_stock.set(" ")
         lbl_stock: Label = Label(top_frame,text='Underlying',justify=LEFT,font=("TkDefaultFont", 10,"bold"))
         #lbl_stock.pack(anchor=N, expand=False, side=LEFT)
-        lbl_stock.grid(row=row_idx,column=0,sticky='nw',padx=pdx,pady=pdy)
-        self.combo_box_stock = Combobox(top_frame,width=10,textvariable=var_stock) 
+        lbl_stock.grid(row=0,column=row_idx,sticky='nw',padx=pdx,pady=pdy)
+        self.combo_box_stock = Combobox(top_frame,width=15,textvariable=var_stock) 
         #self.combo_box_stock.pack(anchor=N, expand=False, side=LEFT)
-        self.combo_box_stock.grid(row=row_idx,column=1,sticky='nw',padx=pdx,pady=pdy)
+        self.combo_box_stock.grid(row=0,column=row_idx+1,sticky='nw',padx=pdx,pady=pdy)
         self.combo_box_stock.configure(state='readonly')
         self.combo_box_stock['values'] = self.root.indices
-        self.combo_box_stock.bind('<<ComboboxSelected>>', self.set_expiry_date)
+        self.combo_box_stock.bind('<<ComboboxSelected>>', self.event_chng_underlying)
         row_idx = row_idx + 1
         
         date_var_stock: StringVar = StringVar()
         date_var_stock.set(" ")
         lbl_exp_date_stock: Label = Label(top_frame,text='Expiry',justify=LEFT,font=("TkDefaultFont", 10,"bold"))
-        lbl_exp_date_stock.grid(row=row_idx,column=0,sticky='nsw',padx=pdx,pady=pdy)
+        lbl_exp_date_stock.grid(row=0,column=row_idx+2,sticky='nsw',padx=pdx,pady=pdy)
         #lbl_exp_date_stock.pack(anchor='nw',fill='y', expand=True, side=TOP)
-        self.cbox_expiry_date = Combobox(top_frame,width=10,textvariable=date_var_stock) 
-        self.cbox_expiry_date.grid(row=row_idx,column=1,sticky='nsw',padx=pdx,pady=pdy)
+        self.cbox_expiry_date = Combobox(top_frame,width=20,textvariable=date_var_stock) 
+        self.cbox_expiry_date.grid(row=0,column=row_idx+3,sticky='nsw',padx=pdx,pady=pdy)
         #self.cbox_expiry_date.pack(anchor='nw',fill='y', expand=True, side=TOP)
         self.cbox_expiry_date.configure(state='readonly')
         self.cbox_expiry_date.bind('<<ComboboxSelected>>', self.event_chng_expiry_date)
         row_idx = row_idx + 1
-        
+        #
         intvl_var_stock: StringVar = StringVar()
         intvl_var_stock.set(" ")
         lbl_intvl: Label = Label(top_frame,text='Refresh (seconds)',justify=LEFT,font=("TkDefaultFont", 10,"bold"))
-        lbl_intvl.grid(row=row_idx,column=0,sticky='nsw',padx=pdx,pady=pdy)
+        lbl_intvl.grid(row=0,column=row_idx+3,sticky='nsw',padx=pdx,pady=pdy)
         self.cbox_intvl = Combobox(top_frame,width=10,textvariable=intvl_var_stock) 
-        self.cbox_intvl.grid(row=row_idx,column=1,sticky='nsw',padx=pdx,pady=pdy)
+        self.cbox_intvl.grid(row=0,column=row_idx+4,sticky='nsw',padx=pdx,pady=pdy)
         self.cbox_intvl.configure(state='readonly')
         self.cbox_intvl['values'] = list(range(10, 600, 20))
         self.cbox_intvl.bind('<<ComboboxSelected>>', self.event_chng_intvl)
         row_idx = row_idx + 1
         
-        
-        #self.start_button: Button = tk.Button(top_frame,text='Fetch OI',command=self.main_recursive,width=15,bg='green',fg='white',font=("TkDefaultFont", 10, "bold"))
-        #self.start_button.grid(row=row_idx,column=0,sticky='nsw',columnspan=2)#,padx=pdx,pady=pdy)
-        #self.start_button.configure(state='disabled')
-        #row_idx = row_idx + 1
         
         fig,ax = plt.subplots(2)
         self.plot1 = ax[0]
@@ -75,7 +70,7 @@ class OI_ANALYSIS:
         self.canvas = FigureCanvasTkAgg(fig,master = bot_frame)
         self.canvas.get_tk_widget().pack(fill='both',expand=True,side=TOP)
     
-    def set_expiry_date(self,event):
+    def event_chng_underlying(self,event):
         self.nse_adapter.set_stock(self.combo_box_stock.get())
         self.nse_adapter.get_expiry_dates()
         self.cbox_expiry_date['values'] = tuple(self.nse_adapter.expiry_dates)
@@ -173,7 +168,8 @@ class OI_ANALYSIS:
         x = bar.get_x()+bar.get_width()/2.
         y = bar.get_y()+bar.get_height()
         max_strike = (self.processed_df['strikePrice'].tolist())[-1]
-        self.annot.xy = (max_strike,y)
+        #self.annot.xy = (max_strike,y)
+        self.annot.xy = (x,y)
         pcr = self.processed_df.loc[self.processed_df['strikePrice']==strike_price]
         pcr = (pcr['PCR'].tolist())[0]
         text = "{:.1f}\nPCR : {:.3f}".format(strike_price,pcr)
@@ -185,7 +181,8 @@ class OI_ANALYSIS:
         x = bar.get_x()+bar.get_width()/2.
         y = bar.get_y()+bar.get_height()
         max_strike = (self.processed_df['strikePrice'].tolist())[-1]
-        self.annot1.xy = (max_strike,y)
+        #self.annot1.xy = (max_strike,y)
+        self.annot1.xy = (x,y)
         pcr = self.processed_df.loc[self.processed_df['strikePrice']==strike_price]
         pcr = (pcr['PCR'].tolist())[0]
         text = "{:.1f}\nPCR : {:.3f}".format(strike_price,pcr)
